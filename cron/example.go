be42dc4ba80a0a1e6d2e24c4a8558bcd9c7bb002
Qianjiachen55 <qianjiachen@happywater.xyz>
2021-10-24 21:17:27 +0800

    fix(cron): add parser

--- cron/example.go
+++ cron/example.go
@@ -6,23 +6,60 @@ import (
 	"time"
 )
 
-func main() {
-	c := cron.New()
+type GreetingJob struct {
+	Name string
+}
 
-	c.AddFunc("@every 10s", func() {
+func (g GreetingJob) Run() {
+	fmt.Println("Hello ", g.Name)
+}
+
+func main() {
+	a := cron.New()
+
+	a.AddFunc("@every 6s", func() {
 		fmt.Println(time.Now())
 	},
 	)
 
-	a := cron.New()
-	a.AddFunc("12-59/1 * * * *", func() {
-		fmt.Println("******************")
-		fmt.Println(time.Now())
-		fmt.Println("******************")
+	//b := cron.New()
+	//b.AddFunc("12-59/1 * * * *", func() {
+	//	fmt.Println("******************")
+	//	fmt.Println(time.Now())
+	//	fmt.Println("******************")
+	//})
+
+	//c := cron.New()
+	//c.AddJob("@every 1s", GreetingJob{Name: "jason"})
+	//
+
+	//parser := cron.NewParser(
+	//	cron.Second | cron.Minute | cron.Dom | cron.Month | cron.Dow | cron.Descriptor,
+	//)
+
+	//d := cron.New(cron.WithParser(parser))
+	//_,err := d.AddFunc("1-59/1 * * * * *", func() {
+	//	fmt.Println("d_func!")
+	//})
+
+
+	d := cron.New(
+		cron.WithParser(
+			cron.NewParser(
+				cron.Second | cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow | cron.Descriptor)))
+	_,err := d.AddFunc("1-59/1 * * * * *", func() {
+		fmt.Println("d_func!")
 	})
+	if err != nil{
+		fmt.Println(err)
+	}
+
+
+	//c.Start()
+	//b.Start()
 	a.Start()
+	d.Start()
 
-
-	c.Start()
+	time.Sleep(time.Hour * 1)
 
 }
